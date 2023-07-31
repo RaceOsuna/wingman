f53a62471e2583cfd9c662caaf240a1315c545da
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-07-31 17:19:02 -0600

    Feat: toggle online status

---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function NewsCard(props) {
+  return (
+    <div>NewsCard</div>
+  )
+}
+
+NewsCard.propTypes = {}
+
+export default NewsCard
--- src/components/PlayerProfile/PlayerProfile.js
+++ src/components/PlayerProfile/PlayerProfile.js
@@ -1,19 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './PlayerProfile.css'
+import { useEffect, useState } from'react'
 
 function PlayerProfile({ playerData }) {
 
+  const [news, setNews] = useState([])
+
+  useEffect(() => {
+    fetch(`https://api.mozambiquehe.re/news?auth=bae15f3f336782882976819cd65d9ef3`)
+    .then(res => res.json())
+    .then(data => setNews(data))
+  }, [])
+
+  // const toggleStatus = () => {
+  //   if (playerData.realtime.currentState === 'online') {
+  //     return 'green'
+  //   } else {
+  //     return 'red'
+  //   }
+  // }
+  
+  const toggleStatus = playerData.realtime.currentState === 'online' ? 'green' : 'red'
+
+  console.log(news)
+
   return (
     <div className='player-profile'>
-      <div className='name-avatar'>
+     <div className='name-avatar'>
         <img src={playerData.global.avatar} />
         <h1>{playerData.global.name}</h1>
       </div>
+      <p className="status" style={{color: toggleStatus}}>{playerData.realtime.currentState}</p>
       <div className='legend-icon'>
         <img src={playerData.legends.selected.ImgAssets.icon} />
       </div>
       <div className='rank-details'>
+        <p>Legend: {playerData.legends.selected.LegendName}</p>
         <p>Rank: {playerData.global.rank.rankName}</p>
         <p>LP: {playerData.global.rank.rankScore}</p>
         <p>Kills: {playerData.total.kills.value}</p>
