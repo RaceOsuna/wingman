791fcd676ea3eacd51856d4fbb5e9c223b9e52e4
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-08-01 12:12:03 -0600

    Feat: search legends

--- src/components/AllLegends/AllLegends.js
+++ src/components/AllLegends/AllLegends.js
@@ -2,12 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import LegendCard from '../LegendCard/LegendCard'
 import './AllLegends.css'
+import { useSearchParams } from 'react-router-dom'
 
 function AllLegends({ playerData }) {
-  // playerData.legends.all[legend]
+  
+  const [searchParams, setSearchParams] = useSearchParams()
+  const filteredLegends = searchParams.get('legends')
+
   const legends = Object.keys(playerData.legends.all).filter(key => key !== 'Global')
-  const allLegends = legends.map(legend => {
-    // console.log(playerData.legends.all[legend].data)
+  const displayLegends = filteredLegends ? legends.filter(legend => legend.toLowerCase().includes(filteredLegends.toLowerCase())) : legends
+  
+  const allLegends = displayLegends.map(legend => {
     let data = playerData.legends.all[legend].data
     let images = playerData.legends.all[legend].ImgAssets
     return (
@@ -20,9 +25,15 @@ function AllLegends({ playerData }) {
     )
   })
   return (
-    <div className='legend-container'>
-      {allLegends}
+    <div className='legend-page'>
+      <div className='search'>
+        <input type='text' name='legends' placeholder='Search Legends' value={filteredLegends} onChange={e => setSearchParams({'legends': e.target.value})} />
+      </div>
+      <div className='legend-container'>
+        {allLegends}
+      </div>
     </div>
+      
   )
 }
 
