32f0c5a1af753030d6bfcdefe93afdcbccf1eb24
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-08-04 15:28:43 -0600

    Feature: cypress error test messages

--- cypress/e2e/Error.cy.js
+++ cypress/e2e/Error.cy.js
@@ -1,6 +1,29 @@
-describe('template spec', () => {
-  
-  beforeEach(() => {
+describe('error messages', () => {
+
+  it('should display 500 level error messages', () => {
+    cy.visit('http://localhost:3000/')
+
+    cy.intercept('GET', 'https://api.mozambiquehe.re/news?auth=bae15f3f336782882976819cd65d9ef3', {
+      statusCode: 500,
+      fixture: 'news'
+    }).as('news')
+
+    cy.intercept('GET', 'https://api.mozambiquehe.re/bridge?auth=bae15f3f336782882976819cd65d9ef3&player=daltooshhhhh&platform=PC', {
+        statusCode: 500,
+        fixture: 'stats'
+      }).as('stats')
+
+    cy.wait('@news')
+      cy.get('button[value="PC"]').click()
+      cy.get('input[name="username"]').type("daltooshhhhh")
+      cy.get('a[name=submit]').click()
+      cy.wait('@stats')
+
+      cy.get('h1').should('be.visible').should('have.text', "500 Oops! Nevermind. The server is down...")
+  })
+
+
+  it('should display 400 level error messages', () => {
     cy.visit('http://localhost:3000/')
 
     cy.intercept('GET', 'https://api.mozambiquehe.re/news?auth=bae15f3f336782882976819cd65d9ef3', {
@@ -12,9 +35,7 @@ describe('template spec', () => {
         statusCode: 404,
         fixture: 'stats'
       }).as('stats')
-  })
-  
-  it('passes', () => {
+
     cy.wait('@news')
       cy.get('button[value="PC"]').click()
       cy.get('input[name="username"]').type("daltooshhhhh")
@@ -22,5 +43,27 @@ describe('template spec', () => {
       cy.wait('@stats')
 
       cy.get('h1').should('be.visible').should('have.text', "404 Oops! Nevermind. This page doesn't exist...")
+  })
+
+  it('should display 300 level error messages', () => {
+    cy.visit('http://localhost:3000/')
+
+    cy.intercept('GET', 'https://api.mozambiquehe.re/news?auth=bae15f3f336782882976819cd65d9ef3', {
+      statusCode: 300,
+      fixture: 'news'
+    }).as('news')
+
+    cy.intercept('GET', 'https://api.mozambiquehe.re/bridge?auth=bae15f3f336782882976819cd65d9ef3&player=daltooshhhhh&platform=PC', {
+        statusCode: 300,
+        fixture: 'stats'
+      }).as('stats')
+
+    cy.wait('@news')
+      cy.get('button[value="PC"]').click()
+      cy.get('input[name="username"]').type("daltooshhhhh")
+      cy.get('a[name=submit]').click()
+      cy.wait('@stats')
+
+      cy.get('h1').should('be.visible').should('have.text', "300 Oops! Nevermind. This page has been moved...")
   })
 })
