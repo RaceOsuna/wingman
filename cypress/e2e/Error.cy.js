fe54437efe3db4aadea66c824c5491f3f141bc7f
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-08-05 11:02:42 -0600

    Feat: error page home button test

--- cypress/e2e/Error.cy.js
+++ cypress/e2e/Error.cy.js
@@ -20,7 +20,7 @@ describe('error messages', () => {
 
       cy.get('h1').should('be.visible').should('have.text', "500")
       cy.get('h2').should('be.visible').should('have.text', "Oops! Nevermind. The server is down...")
-
+      cy.get('a').should('be.visible').should('have.text', "🔄 Return Home")
   })
 
 
@@ -45,6 +45,7 @@ cy.wait('@news')
 
       cy.get('h1').should('be.visible').should('have.text', "404")
       cy.get('h2').should('be.visible').should('have.text', "Oops! Nevermind. This page doesn't exist...")
+      cy.get('a').should('be.visible').should('have.text', "🔄 Return Home")
 
   })
 
@@ -69,5 +70,43 @@ cy.wait('@news')
 
       cy.get('h1').should('be.visible').should('have.text', "300")
       cy.get('h2').should('be.visible').should('have.text', 'Oops! Nevermind. This page has been moved...')
+      cy.get('a').should('be.visible').should('have.text', "🔄 Return Home")
+
+  })
+
+  it('should beable to return to the home page', () => {
+    cy.visit('http://localhost:3000/')
+
+    cy.intercept('GET', 'https://api.mozambiquehe.re/news?auth=bae15f3f336782882976819cd65d9ef3', {
+      statusCode: 200,
+      fixture: 'news'
+    }).as('news')
+
+    cy.intercept('GET', 'https://api.mozambiquehe.re/bridge?auth=bae15f3f336782882976819cd65d9ef3&player=daltooshhhhh&platform=PC', {
+        statusCode: 404,
+        fixture: 'stats'
+      }).as('stats')
+
+cy.wait('@news')
+      cy.get('button[value="PC"]').click()
+      cy.get('input[name="username"]').type("daltooshhhhh")
+      cy.get('a[name=submit]').click()
+      cy.wait('@stats')
+
+      cy.get('h1').should('be.visible').should('have.text', "404")
+      cy.get('h2').should('be.visible').should('have.text', "Oops! Nevermind. This page doesn't exist...")
+      cy.get('a').should('be.visible').should('have.text', "🔄 Return Home")
+
+      cy.get('a').click()
+
+      cy.url().should('eq', 'http://localhost:3000/')
+      cy.get('.form-container').within(() => {
+        cy.contains('h1', 'Wingman').should('be.visible')
+        cy.contains('button', 'PC').should('be.visible')
+        cy.contains('button', 'XBOX').should('be.visible')
+        cy.contains('button', 'PSN').should('be.visible')
+        cy.contains('p', 'Submit').should('be.visible')
+        cy.get('input[name="username"]').should('be.visible').should('have.value', '')
+      })
   })
 })
